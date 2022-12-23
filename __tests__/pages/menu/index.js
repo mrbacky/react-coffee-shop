8f03580f43f7f9e5fda8f6122e51ecbee8945b50
domco545 <43946320+domco545@users.noreply.github.com>
2022-12-23 15:48:24 +0100

    Remove jest add cypress

--- __tests__/pages/menu/index.js
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import Menu from "../../../pages/menu/index";
-
-const PageTitle = "React Coffee";
-test("Page Displays image of coffee shop interior", () => {
-  const page = render(<Menu />);
-});
