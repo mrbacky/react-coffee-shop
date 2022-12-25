8c2fe589773736c524066b55dc7693731b6b3fd9
domco545 <43946320+domco545@users.noreply.github.com>
2022-12-26 00:24:58 +0100

    Comment out mongo connection

--- utils/db/mongodb-client.js
+++ utils/db/mongodb-client.js
@@ -17,16 +17,16 @@ let clientPromise;
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (hot module replacement).
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
+  // if (!global._mongoClientPromise) {
+  //   client = new MongoClient(uri, options);
+  //   global._mongoClientPromise = client.connect();
+  // }
 
-  clientPromise = global._mongoClientPromise;
+  // clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  // client = new MongoClient(uri, options);
+  // clientPromise = client.connect();
 }
 
 // Export a module-scoped MongoClient promise. By doing this in a
