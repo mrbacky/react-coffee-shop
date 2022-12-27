54e744c3269f4322d62b9b3537cb436edb1445f7
domco545 <43946320+domco545@users.noreply.github.com>
2022-12-27 15:23:38 +0100

    Create cart POM

---
+++ cypress/e2e/PageObjectModels/CartPage.ts
@@ -0,0 +1,53 @@
+class CartPage {
+    getClearCartButton(){
+        return cy.get('/html/body/div[2]/div[2]/div/div[2]/button[1]')
+    }
+
+    getCloseButton(){
+        return cy.get('/html/body/div[2]/div[2]/div/div[2]/button[2]')
+    }
+
+    getOrderButton(){
+        return cy.get('/html/body/div[2]/div[2]/div/div[2]/button[3]')
+    }
+
+    getFirstCartItem(){
+        return cy.get('/html/body/div[2]/div[2]/div/ul').find('li').eq(0)
+    }
+
+    getFirstCartItemAmount(){
+        return cy.get('//*[@id="overlays"]/div[2]/div/ul/li[1]/div[1]/div/span[2]')
+    }
+
+    getFirstCartItemRemoveButton(){
+        return cy.get('//*[@id="overlays"]/div[2]/div/ul/li[1]/div[2]/button[1]')
+    }
+
+    getFirstCartItemDecreaseAmountButton(){
+        return cy.get('//*[@id="overlays"]/div[2]/div/ul/li[1]/div[2]/button[2]')
+    }
+
+    getFirstCartItemIncreaseAmountButton(){
+        return cy.get('//*[@id="overlays"]/div[2]/div/ul/li[1]/div[2]/button[3]')
+    }
+
+    getConfirmContinueAsGuestButton(){
+        return cy.get('//*[@id="overlays"]/div[2]/div/div[2]/div/button[1]')
+    }
+
+    getFirstNameInput(){
+        return cy.get('#first-name')
+    }
+
+    getLastNameInput(){
+        return cy.get('#last-name')
+    }
+
+    getEmailInput(){
+        return cy.get('#email')
+    }
+
+    getConfirmOrderButton(){
+        return cy.get('/html/body/div[2]/div[2]/div/form').contains('Confirm')
+    }
+}
