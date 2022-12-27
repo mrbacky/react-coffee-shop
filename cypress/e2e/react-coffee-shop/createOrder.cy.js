56a6cb026d1c62219e73a030c4ff3161326e6eed
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2022-12-27 15:11:10 +0100

    create user during order creation and create order test, installed chance - random data generator

--- cypress/e2e/react-coffee-shop/createOrder.cy.js
+++ cypress/e2e/react-coffee-shop/createOrder.cy.js
@@ -1,21 +1,55 @@
 /// <reference types="cypress" />
 
-describe('Create order', () => {
+import { Chance } from "chance";
+const chance = new Chance();
+
+describe("Create order", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
   });
 
-  it('creates a new order as guest', () => {
-    cy.contains('Menu').click()
-    cy.contains("Add").click()
-    cy.contains("Add").click()
+  it("Creates a new order as a guest", () => {
+    cy.contains("Menu").click();
+    cy.contains("Add").click();
+    cy.contains("Add").click();
+    cy.contains("Your Cart").click();
+    cy.contains("Order").click();
+    cy.contains("Yes").click();
+    cy.contains("First Name").type("Jane");
+    cy.contains("Last Name").type("Doe");
+    cy.contains("Email").type(chance.email());
+    cy.contains("Confirm").click();
+    cy.contains("Your order was created!").should("be.visible");
+  });
+
+  it("Creates user during order creation and creates order", () => {
+    // pick items
+    cy.contains("Menu").click();
+    cy.contains("Add").click();
+    cy.contains("Add").click();
+    cy.contains("Your Cart").click();
+    cy.contains("Order").click();
+    // create account
+    cy.contains("Sign in/Create Account").click();
+    cy.contains("Create a new account").click();
+    cy.contains("First Name").type("test first name");
+    cy.contains("Last Name").type("test last name");
+    const testEmail = chance.email()
+    console.log("test email: ", testEmail)
+    cy.contains("Your Email").type(testEmail);
+    cy.contains("Your Password").type("1234567");
+    cy.contains("Create Account").click();
+    cy.get('button[type="submit"').click()
+    cy.contains("Created new user!").should("be.visible");
+    cy.contains("Please login using your newly created credentials.").should("be.visible");
+    // login
+    cy.contains("Your Email").type(testEmail);
+    cy.contains("Password").type("1234567");
+    cy.get('button').contains("Sign In").click()
+    // create order
     cy.contains("Your Cart").click()
-    cy.contains("Order").click()
-    cy.contains("Yes").click()
-    cy.contains("First Name").type("Jane")
-    cy.contains("Last Name").type("Doe")
-    cy.contains("Email").type("janedoe@gmail.com")
-    cy.contains("Confirm").click()
-    cy.contains('Order created succesfully').should('be.visible')
+    cy.contains("Order").click();
+    cy.contains("Confirm").click();
+    cy.contains("Your order was created!").should("be.visible");
   });
 });
