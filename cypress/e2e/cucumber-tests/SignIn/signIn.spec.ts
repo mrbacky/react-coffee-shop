45791cb25a43ebae86fe135f64d64abcd7391417
domco545 <43946320+domco545@users.noreply.github.com>
2022-12-28 17:30:13 +0100

    Fix cucumber

--- cypress/e2e/cucumber-tests/SignIn/signIn.spec.ts
+++ cypress/e2e/cucumber-tests/SignIn/signIn.spec.ts
@@ -1,24 +1,25 @@
 /// <reference types="cypress" />
-import { Given, Then, And, When } from 'cypress-cucumber-preprocessor/steps'
 import LoginPage from '../../PageObjectModels/LoginPage';
 import Navbar from "../../PageObjectModels/Navbar"
+import {Given, Then, When} from "@badeball/cypress-cucumber-preprocessor";
 
 const navbar = new Navbar();
 const loginPage = new LoginPage();
 
 Given('An user opens login page', () => {
-    cy.visit("/")
+    cy.visit("/auth")
     navbar.getSignInButton().click();
 })
 
-When('the user fills in login form', () => {
+When('the user fills in and submits login form', () => {
     loginPage.getEmailInput().type('mr.backy@gmail.com')
     loginPage.getPasswordInput().type('1234567')
-})
-
-And('submits login form', () => {
     loginPage.getLogInButton().click();
 })
+
+// And('submits login form', () => {
+//     loginPage.getLogInButton().click();
+// })
 
 Then('the user should see homepage with his first name and last name', () => {
     cy.get('#welcome-message')
--- cypress/e2e/cucumber-tests/signIn.feature
+++ cypress/e2e/cucumber-tests/signIn.feature
@@ -5,7 +5,6 @@ Feature: Sign in
 
     Scenario: Valid Sign in
         Given An user opens login page
-        When the user fills in login form
-        And submits login form
+        When the user fills in and submits login form
         Then the user should see homepage with his first name and last name
     
