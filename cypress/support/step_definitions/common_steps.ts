866be8931e3282bd7d938ef03a90389b2f6c4cc9
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2023-01-20 02:16:20 +0100

    making navigation step as common step

--- cypress/support/step_definitions/common_steps.ts
+++ cypress/support/step_definitions/common_steps.ts
@@ -1,4 +1,9 @@
-import { Then } from "@badeball/cypress-cucumber-preprocessor";
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I am on {string} page", (pagePath: string) => {
+  cy.visit("/" + pagePath);
+  cy.url().should("include", pagePath);
+});
 
 Then("I should see {string} message", (message: string) => {
   cy.contains(message).should("be.visible");
--- cypress/support/step_definitions/navigation_steps.ts
+++
@@ -1,6 +0,0 @@
-import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
-
-Given("I am on {string} page", (pagePath: string) => {
-  cy.visit("/" + pagePath);
-  cy.url().should("include", pagePath);
-});
